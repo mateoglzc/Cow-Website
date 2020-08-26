328c16274217f72a2f95a2e5bfa74c774fe8ecf2
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2020-08-25 21:52:38 -0500

    Advanced template

---
+++ moo.py
@@ -0,0 +1,6 @@
+
+# I don't know if this accuretly represents how many cows how you eaten in your life time 
+def moo(beef_per_week, years):
+    years -= 4
+    years_weeks = 52*years
+    return ((years_weeks*200*beef_per_week)/204117)
